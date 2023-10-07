3192cd52f7936c9c8330d3c67db17b1a162105cb
Meriem-BM <barhoumi.meriem1@gmail.com>
2023-10-07 20:22:45 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::prelude::*;
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -6,12 +6,19 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-                stream.flush();
+            Ok(mut _stream) => {
+                // Prepare the HTTP response
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+                // Write the response to the client
+                if let Err(e) = _stream.write_all(response.as_bytes()) {
+                    println!("Error writing to client: {}", e);
+                } else {
+                    println!("Accepted new connection: {:?}", _stream);
+                }
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error accepting connection: {}", e);
             }
         }
     }
