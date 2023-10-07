64d35eafc8efce9f11c5e61cd7bcb9757fc92108
Meriem-BM <barhoumi.meriem1@gmail.com>
2023-10-07 20:26:58 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,22 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                // Read the request data into a buffer
                 let mut buffer = [0; 1024];
                 if let Ok(bytes_read) = _stream.read(&mut buffer) {
-                    // Convert the bytes to a string
                     if let Ok(request_str) = str::from_utf8(&buffer[0..bytes_read]) {
-                        // Parse the request to extract the path
                         if let Some(path) = parse_request_path(request_str) {
-                            // Prepare the HTTP response
-                            let response = if path == "/" {
-                                "HTTP/1.1 200 OK\r\n\r\n"
-                            } else {
-                                "HTTP/1.1 404 Not Found\r\n\r\n"
-                            };
+                            if let Some(random_string) = extract_random_string(&path) {
+                                let response = format!(
+                                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
+                                    random_string.len(),
+                                    random_string
+                                );
 
-                            // Write the response to the client
-                            if let Err(e) = _stream.write_all(response.as_bytes()) {
-                                println!("Error writing to client: {}", e);
-                            } else {
-                                println!("Accepted new connection: {:?}", _stream);
+                                if let Err(e) = _stream.write_all(response.as_bytes()) {
+                                    println!("Error writing to client: {}", e);
+                                } else {
+                                    println!("Accepted new connection: {:?}", _stream);
+                                }
                             }
                         }
                     }
@@ -40,18 +37,24 @@ fn main() {
 }
 
 fn parse_request_path(request: &str) -> Option<&str> {
-    // Split the request into lines
     let lines: Vec<&str> = request.lines().collect();
 
-    // Check if the request has a start line
     if let Some(start_line) = lines.get(0) {
-        // Split the start line into parts
         let parts: Vec<&str> = start_line.split_whitespace().collect();
 
-        // Check if there are enough parts
         if parts.len() >= 2 {
-            // The path is the second part of the start line
             return Some(parts[1]);
+        }
+    }
+
+    None
+}
+
+fn extract_random_string(path: &str) -> Option<&str> {
+    let mut parts = path.split('/');
+    if parts.next() == Some("") && parts.next() == Some("echo") {
+        if let Some(random_string) = parts.next() {
+            return Some(random_string);
         }
     }
 
