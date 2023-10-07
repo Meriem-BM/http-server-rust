7a2741d39d843c46fff63699f93c4a24a1d21710
Meriem-BM <barhoumi.meriem1@gmail.com>
2023-10-07 20:24:02 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::Write;
+use std::io::{prelude::*, Write};
 use std::net::TcpListener;
+use std::str;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,14 +8,28 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                // Prepare the HTTP response
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                // Read the request data into a buffer
+                let mut buffer = [0; 1024];
+                if let Ok(bytes_read) = _stream.read(&mut buffer) {
+                    // Convert the bytes to a string
+                    if let Ok(request_str) = str::from_utf8(&buffer[0..bytes_read]) {
+                        // Parse the request to extract the path
+                        if let Some(path) = parse_request_path(request_str) {
+                            // Prepare the HTTP response
+                            let response = if path == "/" {
+                                "HTTP/1.1 200 OK\r\n\r\n"
+                            } else {
+                                "HTTP/1.1 404 Not Found\r\n\r\n"
+                            };
 
-                // Write the response to the client
-                if let Err(e) = _stream.write_all(response.as_bytes()) {
-                    println!("Error writing to client: {}", e);
-                } else {
-                    println!("Accepted new connection: {:?}", _stream);
+                            // Write the response to the client
+                            if let Err(e) = _stream.write_all(response.as_bytes()) {
+                                println!("Error writing to client: {}", e);
+                            } else {
+                                println!("Accepted new connection: {:?}", _stream);
+                            }
+                        }
+                    }
                 }
             }
             Err(e) => {
@@ -22,4 +37,23 @@ fn main() {
             }
         }
     }
+}
+
+fn parse_request_path(request: &str) -> Option<&str> {
+    // Split the request into lines
+    let lines: Vec<&str> = request.lines().collect();
+
+    // Check if the request has a start line
+    if let Some(start_line) = lines.get(0) {
+        // Split the start line into parts
+        let parts: Vec<&str> = start_line.split_whitespace().collect();
+
+        // Check if there are enough parts
+        if parts.len() >= 2 {
+            // The path is the second part of the start line
+            return Some(parts[1]);
+        }
+    }
+
+    None
 }
